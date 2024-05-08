0978f2f34decc5ecd29f7cb4c7f14bf3a43eb682
Ernesto Freiberg <efreibe@gmail.com>
2024-05-07 21:33:03 -0300

    Te firmé el contenido que me pediste

--- solicitudes/envioSobre.js
+++ solicitudes/envioSobre.js
@@ -1,12 +1,14 @@
-import { crearCliente, setClientSecurity, ejecutarSolicitudSoap } from '../lib/soap.js'; 
-import fs from 'fs'
-import { parseString } from 'xml2js';
-import { incrementarIdEmisor } from '../lib/tools.js';
-import { firmarXml } from '../lib/firmarXML.js';
+import 'dotenv/config'
+
+import fs, { writeFileSync } from 'fs'
+import { parseString } from 'xml2js'
+import { incrementarIdEmisor } from '../lib/tools.js'
+import { crearCliente, firmarXml, setClientSecurity, ejecutarSolicitudSoap } from '../lib/soap.js'
 
 const sobre_prueba = '../sobre-prueba.xml'
-const signed = '../signed.xml'
+
 const certificado = '../certificados/La_Riviera.pfx'
+
 const url = 'https://efactura.dgi.gub.uy:6443/ePrueba/ws_eprueba?wsdl' //testing
 
 const cliente = await crearCliente(url, {})
@@ -14,33 +16,23 @@ const cliente = await crearCliente(url, {})
 setClientSecurity(cliente, certificado, process.env.PASSWORD)
 
 await incrementarIdEmisor(sobre_prueba)
-//firmarXml (sobre_prueba)
 
-const xmlData = fs.readFileSync(sobre_prueba, 'utf8');
+const xmlData = fs.readFileSync(sobre_prueba, 'utf8')
 
-//el problema que veo es que calculo que se me firma la solicitud pero necesito que se firme el xmlData o algo asi
+const args = firmarXml(xmlData, certificado, process.env.PASSWORD)
 
-const args = {
-    Datain: {
-        xmlData: xmlData
-    } 
-};
+writeFileSync('../sobre-firmado.xml', args.Datain.xmlData, 'utf8')
 
+const resultado = await ejecutarSolicitudSoap(cliente, 'EFACRECEPCIONSOBREAsync', args)
 
-const resultado = await ejecutarSolicitudSoap(cliente, "EFACRECEPCIONSOBREAsync", args)
-
-
-console.log("----------SOBRE-----------\n")
-
+console.log('----------SOBRE-----------\n')
 
 parseString(resultado[0].Dataout.xmlData, (err, result) => {
-    if (err) {
-        console.error('Error al parsear el XML:', err);
-    } else {
-        console.log("Caratula SOBRE: ", JSON.stringify(result.ACKSobre.Caratula))  
-        console.log("\n")      
-        console.log("Detalle SOBRE: ", JSON.stringify(result.ACKSobre.Detalle))        
-    }
-  });
-
-
+  if (err) {
+    console.error('Error al parsear el XML:', err)
+  } else {
+    console.log('Caratula SOBRE: ', JSON.stringify(result.ACKSobre.Caratula))
+    console.log('\n')
+    console.log('Detalle SOBRE: ', JSON.stringify(result.ACKSobre.Detalle))
+  }
+})
--- solicitudes/estadoEnvio.js
+++ solicitudes/estadoEnvio.js
@@ -1,8 +1,8 @@
-import { crearCliente, setClientSecurity, ejecutarSolicitudSoap } from './index.js'; 
+import 'dotenv/config'
+
 import fs from 'fs'
-import { parseString } from 'xml2js';
-
-
+import { parseString } from 'xml2js'
+import { crearCliente, setClientSecurity, ejecutarSolicitudSoap } from './index.js'
 
 const url = 'https://efactura.dgi.gub.uy:6443/ePrueba/ws_eprueba?wsdl' //testing
 
@@ -10,25 +10,24 @@ const cliente = await crearCliente(url, {})
 
 setClientSecurity(cliente, './certificados/La_Riviera.pfx', process.env.PASSWORD)
 
-console.log("----------CFEs-----------")
+console.log('----------CFEs-----------')
 
-const xmlData = fs.readFileSync('token.xml', 'utf8');
+const xmlData = fs.readFileSync('token.xml', 'utf8')
 
 const args = {
   Datain: {
-      xmlData: xmlData
-  } 
-};
+    xmlData: xmlData,
+  },
+}
 
-const resultado = await ejecutarSolicitudSoap(cliente, "EFACCONSULTARESTADOENVIOAsync", args)
-
+const resultado = await ejecutarSolicitudSoap(cliente, 'EFACCONSULTARESTADOENVIOAsync', args)
 
 parseString(resultado[0].Dataout.xmlData, (err, result) => {
-    if (err) {
-        console.error('Error al parsear el XML:', err);
-    } else {
-        console.log(result)//Puede pasar que no se procese en el momento y tarde un toque en dar la respuesta
-        console.log("Caratula SOBRE: ", JSON.stringify(result.ACKCFE?.Caratula))        
-        console.log("Detalle SOBRE: ", JSON.stringify(result.ACKCFE?.ACKCFE_Det[0]))//esto debe ser un array porque pueden ser varios CFEs        
-    }
-  });
+  if (err) {
+    console.error('Error al parsear el XML:', err)
+  } else {
+    console.log(result) //Puede pasar que no se procese en el momento y tarde un toque en dar la respuesta
+    console.log('Caratula SOBRE: ', JSON.stringify(result.ACKCFE?.Caratula))
+    console.log('Detalle SOBRE: ', JSON.stringify(result.ACKCFE?.ACKCFE_Det[0])) //esto debe ser un array porque pueden ser varios CFEs
+  }
+})

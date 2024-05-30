80ccfe1c2f688e2d6d928249a65a770822978621
MateoF01 <mateofernandez380@gmail.com>
2024-05-30 10:02:29 -0300

    DGI acepta sobre

--- solicitudes/envioSobre.js
+++ solicitudes/envioSobre.js
@@ -4,26 +4,47 @@ import fs, { writeFileSync } from 'fs'
 import { parseString } from 'xml2js'
 import { incrementarIdEmisor, actualizarToken } from '../lib/tools.js'
 import { crearCliente, firmarXml, setClientSecurity, ejecutarSolicitudSoap } from '../lib/soap.js'
+import { DOMParser, XMLSerializer } from 'xmldom'
+import { select } from 'xpath'
 
-const sobre_prueba = '../sobre-prueba.xml'
 
+
+const CFE_prueba = '../CFE-prueba.xml'
+const sobre_prueba = '../sobre-base.xml'
 const certificado = '../certificados/La_Riviera.pfx'
 
+// Seteo cliente
 const url = 'https://efactura.dgi.gub.uy:6443/ePrueba/ws_eprueba?wsdl' //testing
-
 const cliente = await crearCliente(url, {})
-
 setClientSecurity(cliente, certificado, process.env.PASSWORD)
+
+// Firmo CFE
+const xmlData = fs.readFileSync(CFE_prueba, 'utf8')
+const CFE_firmado = firmarXml(xmlData, certificado, process.env.PASSWORD)
+
+writeFileSync('../CFE-firmado.xml', CFE_firmado, 'utf8')
+
+// Inserto CFE en sobre
 
 await incrementarIdEmisor(sobre_prueba)
 
-const xmlData = fs.readFileSync(sobre_prueba, 'utf8')
+const sobre = fs.readFileSync(sobre_prueba, 'utf8');
+const baseDoc = new DOMParser().parseFromString(sobre, 'text/xml');
 
-const args = firmarXml(xmlData, certificado, process.env.PASSWORD)
+const fragmentDoc = new DOMParser().parseFromString(CFE_firmado, 'text/xml');
 
-writeFileSync('../sobre-firmado.xml', args.Datain.xmlData, 'utf8')
+const cfeNode = select("//*[local-name(.)='EnvioCFE']", baseDoc)[0];
+const importedNode = baseDoc.importNode(fragmentDoc.documentElement, true);
+cfeNode.appendChild(importedNode);
 
-const resultado = await ejecutarSolicitudSoap(cliente, 'EFACRECEPCIONSOBREAsync', args)
+const serializer = new XMLSerializer();
+const updatedXmlContent = serializer.serializeToString(baseDoc);
+
+fs.writeFileSync('../sobre-final.xml', updatedXmlContent);
+
+// Envio a la DGI
+
+const resultado = await ejecutarSolicitudSoap(cliente, 'EFACRECEPCIONSOBREAsync', { Datain: { xmlData: updatedXmlContent} })
 
 console.log('----------SOBRE-----------\n')
 

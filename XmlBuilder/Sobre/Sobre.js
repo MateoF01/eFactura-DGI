ae586c59ebfe0e102862936cf82f95ce348d8c40
MateoF01 <mateofernandez380@gmail.com>
2024-06-04 17:53:46 -0300

    insertarSobre

--- XmlBuilder/Sobre/Sobre.js
+++ XmlBuilder/Sobre/Sobre.js
@@ -1,3 +1,5 @@
+import { DOMParser, XMLSerializer } from 'xmldom'
+import { select } from 'xpath'
 import {XmlBuilder} from '../XmlBuilder.js'
 
 class Sobre extends XmlBuilder {
@@ -15,6 +17,34 @@ class Sobre extends XmlBuilder {
         </DGICFE:EnvioCFE>`
           return sobreBase
         }
+
+    insertarCFE(sobreXml, CFE_firmado){//refactorizar con menos librerias
+
+        console.log("SOBREXML: ", sobreXml)
+        console.log("CFE_FIRMADO: ", CFE_firmado)
+        //la logica sería appendear el cfe firmado antes de </DGICFE:EnvioCFE>
+        const finalSobre = "</DGICFE:EnvioCFE>"
+        const index = sobreXml.indexOf(finalSobre)
+        console.log(index)
+        const posicionAInsertar = index - 1
+
+        const updatedXmlContent = sobreXml.slice(0, posicionAInsertar) + CFE_firmado 
+                            + sobreXml.slice(posicionAInsertar)
+
+
+        /*
+        const baseDoc = new DOMParser().parseFromString(sobreXml, 'text/xml');
+        const fragmentDoc = new DOMParser().parseFromString(CFE_firmado, 'text/xml');
+        
+        const cfeNode = select("//*[local-name(.)='EnvioCFE']", baseDoc)[0];
+        const importedNode = baseDoc.importNode(fragmentDoc.documentElement, true);
+        cfeNode.appendChild(importedNode);
+        
+        const serializer = new XMLSerializer();
+        const updatedXmlContent = serializer.serializeToString(baseDoc);
+        */
+        return updatedXmlContent
+    }
 
 }
 
--- solicitudes/envioSobre.js
+++ solicitudes/envioSobre.js
@@ -4,12 +4,9 @@ import fs, { writeFileSync } from 'fs'
 import { parseString } from 'xml2js'
 import { incrementarIdEmisor, actualizarToken } from '../lib/tools.js'
 import { crearCliente, firmarXml, setClientSecurity, ejecutarSolicitudSoap } from '../lib/soap.js'
-import { DOMParser, XMLSerializer } from 'xmldom'
-import { select } from 'xpath'
 
-import {CFEFactory} from '../XmlBuilder/CFEFactory.js'
+import { CFEFactory } from '../XmlBuilder/CFEFactory.js'
 import { Sobre } from '../XmlBuilder/Sobre/Sobre.js'
-
 
 const certificado = '../certificados/La_Riviera.pfx'
 const jsonSobre = '../samples/jsons/sobre-base.json'
@@ -28,35 +25,28 @@ const args_sobre = JSON.parse(fs.readFileSync(jsonSobre, 'utf8'))
 const sobreBuilder = new Sobre();
 let sobreXml = sobreBuilder.buildXml(args_sobre)
 
-
-// Construyo y Firmo CFE
-
 const argsCFE = JSON.parse(fs.readFileSync(jsonEFact, 'utf8'))
+// Construyo y Firmo CFE
+for (let i = 0; i < 5; i++){
+  const factory = new CFEFactory();
+  const builder = await factory.createBuilder('eFact'); // yo le paso a la fabrica el cfe que quiero (será un for de cfes)
+  
+  const xmlCFE = builder.buildXml(argsCFE);
+  const CFE_firmado = firmarXml(xmlCFE, certificado, process.env.PASSWORD)
+  
+  // Inserto CFE en sobre
+  
+  sobreXml = sobreBuilder.insertarCFE(sobreXml, CFE_firmado)
 
-const factory = new CFEFactory();
-const builder = await factory.createBuilder('eFact'); // yo le paso a la fabrica el cfe que quiero (será un for de cfes)
+}
 
-const xmlCFE = builder.buildXml(argsCFE);
-const CFE_firmado = firmarXml(xmlCFE, certificado, process.env.PASSWORD)
 
-// Inserto CFE en sobre
 
-const baseDoc = new DOMParser().parseFromString(sobreXml, 'text/xml');
-
-const fragmentDoc = new DOMParser().parseFromString(CFE_firmado, 'text/xml');
-
-const cfeNode = select("//*[local-name(.)='EnvioCFE']", baseDoc)[0];
-const importedNode = baseDoc.importNode(fragmentDoc.documentElement, true);
-cfeNode.appendChild(importedNode);
-
-const serializer = new XMLSerializer();
-const updatedXmlContent = serializer.serializeToString(baseDoc);
-
-fs.writeFileSync('../sobre-final.xml', updatedXmlContent);
+fs.writeFileSync('../sobre-final.xml', sobreXml);
 
 // Envio a la DGI
 
-const resultado = await ejecutarSolicitudSoap(cliente, 'EFACRECEPCIONSOBREAsync', { Datain: { xmlData: updatedXmlContent} })
+const resultado = await ejecutarSolicitudSoap(cliente, 'EFACRECEPCIONSOBREAsync', { Datain: { xmlData: sobreXml} })
 
 console.log('----------SOBRE-----------\n')
 
